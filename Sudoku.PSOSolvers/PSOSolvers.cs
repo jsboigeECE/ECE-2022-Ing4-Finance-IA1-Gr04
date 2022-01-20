b56ed92dd469defd25cc24aa771ef20b2294d535
Karim R-hd <karim@Karims-MacBook-Pro.local>
2022-01-20 17:23:38 +0100

    Ajout PSOsolver

---
+++ Sudoku.PSOSolvers/PSOSolvers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Sudoku.Shared;
+
+namespace Sudoku.PSOSolvers
+{
+    public class PSOSolver1 : Sudoku.Shared.ISolverSudoku
+
+    {
+        public GridSudoku Solve(GridSudoku s)
+        {
+            return s;
+        }
+    }
+}
+
