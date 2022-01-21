0f873fc89513399224c0386f3e877e40770d4e02
Chea Bryan <cebeka75@gmail.com>
2022-01-21 15:48:41 +0100

    Update SASSolvers1.py

--- Sudoku.SimulatedAnnealingSolvers/Resources/SASSolvers1.py
+++ Sudoku.SimulatedAnnealingSolvers/Resources/SASSolvers1.py
@@ -1,1 +1,16 @@
-﻿
+﻿#sudoku instance, we use '0' for empty cells
+instance = ((0,0,0,0,9,4,0,3,0),
+            (0,0,0,5,1,0,0,0,7),
+            (0,8,9,0,0,0,0,4,0),
+            (0,0,0,0,0,0,2,0,8),
+            (0,6,0,2,0,1,0,5,0),
+            (1,0,2,0,0,0,0,0,0),
+            (0,7,0,0,0,0,5,2,0),
+            (9,0,0,0,6,5,0,0,0),
+            (0,4,0,9,7,0,0,0,0))
+
+            #r = [ [ m.evaluate(X[i][j]).as_long() for j in range(9) ] A adapter à la mathode PSO
+   #solution a faire 
+          #for i in range(9) ]
+
+  
