0b8437d4535249cbb39b1f6aa080de06563f7f7b
Chea Bryan <cebeka75@gmail.com>
2022-01-21 15:02:54 +0100

    Ajout Recruit simule

---
+++ Sudoku.SimulatedAnnealingSolvers/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Sudoku.Shared;
+
+namespace Sudoku.SimulatedAnnealingSolvers
+{
+    public class SolversSimulatedAnnealing1 : Shared.ISolverSudoku
+
+    {
+
+        public GridSudoku Solve(GridSudoku s)
+        {
+            return s;
+        }
+    }
+}
