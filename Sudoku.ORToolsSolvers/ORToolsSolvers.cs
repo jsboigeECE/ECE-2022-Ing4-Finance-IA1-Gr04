478aa439fe56f12ae9c5f54a0cd77310330d8c77
alexdutheil <alexandre.dutheil@edu.ece.fr>
2022-01-24 10:06:44 +0100

    Ajout solver OR Tools

---
+++ Sudoku.ORToolsSolvers/ORToolsSolvers.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Sudoku.Shared;
+
+namespace Sudoku.ORToolsSolvers
+{
+    public class ORToolsSolver1 : Sudoku.Shared.ISolverSudoku
+    {
+        public GridSudoku Solve(GridSudoku s)
+        {
+            return s;
+        }
+    }
+}
