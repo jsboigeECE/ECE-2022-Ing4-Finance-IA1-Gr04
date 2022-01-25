0036747056dae5704982e7116574b934b7daabf1
JorgeBelmonte75017 <jorgebel1070@gmail.com>
2022-01-25 15:36:30 +0100

    Envoie de mon 1er Push au Groupe de Projet

--- Sudoku.PSOSolvers/PSOSolvers.cs
+++ Sudoku.PSOSolvers/PSOSolvers.cs
@@ -2,55 +2,61 @@
 using Python.Runtime;
 using Sudoku.Shared;
 
-namespace Sudoku.PSOSolvers
+namespace Sudoku.PSOSolvers  //Ceci est un test
 {
-    //public class PSOSolver1 : Sudoku.Shared.ISolverSudoku
-
-    //{
-    //    public GridSudoku Solve(GridSudoku s)
-    //    {
-    //        return s;
-    //    }
-    //}
-
-    public class PSOSolvers1 : PythonSolverBase
+    public class PSOSolver1 : Sudoku.Shared.ISolverSudoku
     {
-
-
-        public override Shared.GridSudoku Solve(Shared.GridSudoku s)
+        public GridSudoku Solve(GridSudoku s)
         {
-
-            //using (Py.GIL())
-            //{
-            // create a Python scope
-            using (PyModule scope = Py.CreateScope())
-            {
-                // convert the Person object to a PyObject
-                PyObject pyCells = s.Cellules.ToPython();
-
-                // create a Python variable "person"
-                scope.Set("instance", pyCells);
-
-                // the person object may now be used in Python
-                string code = Resources.PSOSolvers1_py;
-                scope.Exec(code);
-                var result = scope.Get("r");
-                var managedResult = result.As<int[][]>();
-                //var convertesdResult = managedResult.Select(objList => objList.Select(o => (int)o).ToArray()).ToArray();
-                return new Shared.GridSudoku() { Cellules = managedResult };
-            }
-            //}
-
+            return s;
         }
+    }
 
-        protected override void InitializePythonComponents()
-        {
-            //InstallPipModule("z3-solver"); A adapter au modèle PSO
-            base.InitializePythonComponents();
-        }
-
-
+    public class Compteur
+    {
+        public CounterChiffres()
 
     }
+
+    
+    //public class PSOSolvers1 : PythonSolverBase
+    //{
+
+
+    //    public override Shared.GridSudoku Solve(Shared.GridSudoku s)
+    //    {
+
+    //        //using (Py.GIL())
+    //        //{
+    //        // create a Python scope
+    //        using (PyModule scope = Py.CreateScope())
+    //        {
+    //            // convert the Person object to a PyObject
+    //            PyObject pyCells = s.Cellules.ToPython();
+
+    //            // create a Python variable "person"
+    //            scope.Set("instance", pyCells);
+
+    //            // the person object may now be used in Python
+    //            string code = Resources.PSOSolvers1_py;
+    //            scope.Exec(code);
+    //            var result = scope.Get("r");
+    //            var managedResult = result.As<int[][]>();
+    //            //var convertesdResult = managedResult.Select(objList => objList.Select(o => (int)o).ToArray()).ToArray();
+    //            return new Shared.GridSudoku() { Cellules = managedResult };
+    //        }
+    //        //}
+
+    //    }
+
+    //    protected override void InitializePythonComponents()
+    //    {
+    //        //InstallPipModule("z3-solver"); A adapter au modèle PSO
+    //        base.InitializePythonComponents();
+    //    }
+
+
+
+    //}
 }
 
