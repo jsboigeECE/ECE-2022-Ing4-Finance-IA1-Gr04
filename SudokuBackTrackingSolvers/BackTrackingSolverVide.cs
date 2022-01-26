8b2d1c71d49585b22a515381636f364e36822828
morganedlas <49777243+morganedlas@users.noreply.github.com>
2022-01-26 18:14:04 +0100

    Update BackTrackingSolverVide.cs

--- SudokuBackTrackingSolvers/BackTrackingSolverVide.cs
+++ SudokuBackTrackingSolvers/BackTrackingSolverVide.cs
@@ -19,7 +19,11 @@ namespace SudokuBackTrackingSolvers
 				// we are trying to
 				// place is already present in
 				// that row, return false;
+<<<<<<< Updated upstream
 				if (s.Cellules[row][d] == num)
+=======
+				if (s.Cellules[row][ d] == num)
+>>>>>>> Stashed changes
 				{
 					return false;
 				}
@@ -28,11 +32,19 @@ namespace SudokuBackTrackingSolvers
 			// Column has the unique numbers (column-clash)
 			for (int r = 0; r < s.Cellules.GetLength(0); r++)
 			{
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 				// Check if the number
 				// we are trying to
 				// place is already present in
 				// that column, return false;
+<<<<<<< Updated upstream
 				if (s.Cellules[r][col] == num)
+=======
+				if (s.Cellules[r][ col] == num)
+>>>>>>> Stashed changes
 				{
 					return false;
 				}
@@ -40,7 +52,11 @@ namespace SudokuBackTrackingSolvers
 
 			// corresponding square has
 			// unique number (box-clash)
+<<<<<<< Updated upstream
 			int sqrt = (int)Math.Sqrt(s.Cellules.GetLength(0));
+=======
+			int sqrt = (int)Math.Sqrt(s.Cellules.GetLength(0)) ;
+>>>>>>> Stashed changes
 			int boxRowStart = row - row % sqrt;
 			int boxColStart = col - col % sqrt;
 
@@ -50,7 +66,11 @@ namespace SudokuBackTrackingSolvers
 				for (int d = boxColStart;
 					d < boxColStart + sqrt; d++)
 				{
+<<<<<<< Updated upstream
 					if (s.Cellules[r][d] == num)
+=======
+					if (s.Cellules[r][ d] == num)
+>>>>>>> Stashed changes
 					{
 						return false;
 					}
@@ -60,9 +80,88 @@ namespace SudokuBackTrackingSolvers
 			// if there is no clash, it's safe
 			return true;
 		}
+<<<<<<< Updated upstream
 		public GridSudoku Solve(GridSudoku s)
+=======
+
+		public bool Solve(GridSudoku s)
+>>>>>>> Stashed changes
         {
-            return s;
-        }
+			int row = -1;
+			int col = -1;
+			bool isEmpty = true;
+			for (int i = 0; i < 9; i++)
+			{
+				for (int j = 0; j < 9; j++)
+				{
+					if (s.Cellules[i] [j] == 0)
+					{
+						row = i;
+						col = j;
+
+						// We still have some remaining
+						// missing values in Sudoku
+						isEmpty = false;
+						break;
+					}
+				}
+				if (!isEmpty)
+				{
+					break;
+				}
+			}
+
+			// no empty space left
+			if (isEmpty)
+			{
+				return true;
+			}
+
+			// else for each-row backtrack
+			for (int num = 1; num <= 9; num++)
+			{
+				if (isSafe(s, row, col, num))
+				{
+					s.Cellules[row][ col] = num;
+					if (Solve(s))
+					{
+
+						// Print(board, n);
+						return true;
+
+					}
+					else
+					{
+
+						// Replace it
+						s.Cellules[row][ col] = 0;
+					}
+				}
+			}
+			return false;
+			
+		}
+
+	
+
+
+
+        GridSudoku ISolverSudoku.Solve(GridSudoku s)
+        {
+
+			if (Solve(s))
+			{
+
+				return s;
+			}
+			else
+			{
+				Console.Write("No solution");
+				return s;
+
+			}
+		}
     }
+
 }
+
