725aab97f53782096d376cdaf38f8154f2d16761
Spydie78 <guillaume.sima@edu.ece.fr>
2022-01-27 19:07:28 +0100

    Début CSPSolver

--- Sudoku.CSPSolvers/CSPSolevrVide.cs
+++ Sudoku.CSPSolvers/CSPSolevrVide.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Python.Runtime;
 using Sudoku.Shared;
 
 namespace Sudoku.CSPSolvers
@@ -9,5 +10,35 @@ namespace Sudoku.CSPSolvers
         {
             return s;
         }
+    
+        /* public GridSudoku Solve(GridSudoku s)
+        {
+            //System.Diagnostics.Debugger.Break();
+
+            //For some reason, the Benchmark runner won't manage to get the mutex whereas individual execution doesn't cause issues
+            //using (Py.GIL())
+            //{
+            // create a Python scope
+            using (PyModule scope = Py.CreateScope())
+            {
+                // convert the Person object to a PyObject
+                PyObject pySudoku = s.ToPython();
+
+                // create a Python variable "person"
+                scope.Set("sudoku", pySudoku);
+
+                // the person object may now be used in Python
+                string code = Resources.SelfCallSolver_py;
+                scope.Exec(code);
+                var result = scope.Get("solvedSudoku");
+                var toReturn = result.As<Shared.GridSudoku>();
+                return toReturn;
+            }
+            //}
+
+        }
+        */
     }
+
+    
 }
