82972d0cd5bc324e5f883a57a667bdb317b65630
alexdutheil <alexandre.dutheil@edu.ece.fr>
2022-01-28 08:57:37 +0100

    Ajout NuGet package OR Tools

--- Sudoku.ORToolsSolvers/ORToolsSolvers.cs
+++ Sudoku.ORToolsSolvers/ORToolsSolvers.cs
@@ -1,52 +1,28 @@
 ﻿using System;
 using Sudoku.Shared;
+using Google.OrTools.ConstraintSolver;
+
 
 namespace Sudoku.ORToolsSolvers
 {
+
     public class ORToolsSolver1 : Sudoku.Shared.ISolverSudoku
     {
+
         public GridSudoku Solve(GridSudoku s)
         {
+
             return s;
+
         }
+
     }
+
 }
 
 
 
 
-//
-// Copyright 2012 Hakan Kjellerstrand
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text.RegularExpressions;
-using Google.OrTools.ConstraintSolver;
-
-
-public class Sudoku
-{
-
-    /**
-     *
-     * Solves a Sudoku problem.
-     *
-     */
     private static void Solve()
     {
         Solver solver = new Solver("Sudoku");
@@ -58,17 +34,6 @@ public class Sudoku
         IEnumerable<int> CELL = Enumerable.Range(0, cell_size);
         int n = cell_size * cell_size;
         IEnumerable<int> RANGE = Enumerable.Range(0, n);
-
-        // 0 marks an unknown value
-        int[,] initial_grid = {{0, 6, 0, 0, 5, 0, 0, 2, 0},
-                           {0, 0, 0, 3, 0, 0, 0, 9, 0},
-                           {7, 0, 0, 6, 0, 0, 0, 1, 0},
-                           {0, 0, 6, 0, 3, 0, 4, 0, 0},
-                           {0, 0, 4, 0, 7, 0, 1, 0, 0},
-                           {0, 0, 5, 0, 9, 0, 8, 0, 0},
-                           {0, 4, 0, 0, 0, 1, 0, 0, 6},
-                           {0, 3, 0, 0, 0, 8, 0, 0, 0},
-                           {0, 2, 0, 0, 4, 0, 0, 5, 0}};
 
 
         //
@@ -158,6 +123,8 @@ public class Sudoku
         Solve();
     }
 }
+
+
 
 
 // https://developers.google.com/optimization/introduction/dotnet
