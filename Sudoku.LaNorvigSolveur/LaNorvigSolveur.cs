d7fb8cf20151992c6871a17c215698c7870548cb
Cléo <cleocabaret@gmail.com>
2022-01-31 17:37:08 +0100

    Tentative recupe valeur dico

--- Sudoku.LaNorvigSolveur/LaNorvigSolveur.cs
+++ Sudoku.LaNorvigSolveur/LaNorvigSolveur.cs
@@ -1,6 +1,8 @@
 ﻿using Sudoku.Shared;
 using System.Text;
 using System.Linq;
+using System;
+using System.Collections.Generic;
 
 namespace Sudoku.LaNorvigSolveur
 {
@@ -11,8 +13,17 @@ namespace Sudoku.LaNorvigSolveur
             var sAuBonFormat = s.Cellules.Select(r => r.Select(c => c == 0 ? "." : c.ToString())
             .Aggregate("", (s1, s2) => s1 + s2, s => s))
                 .Aggregate("", (s1, s2) => s1 + s2, s => s);
-           var sudokuDICO   = LinqSudokuSolver.parse_grid(sAuBonFormat);
-           var solution= LinqSudokuSolver.search(sudokuDICO);
+            var sudokuDICO = LinqSudokuSolver.parse_grid(sAuBonFormat);
+
+            var solution = LinqSudokuSolver.search(sudokuDICO);
+            List<String> lstval = new List<string>();
+            foreach (KeyValuePair<string, string> kvp in solution)
+            {
+                string valeur = Convert.ToString(kvp.Value);
+                lstval.Add(valeur);
+                //Console.WriteLine("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
+            }
+            Console.Write(lstval);
             return s;
         }
 
