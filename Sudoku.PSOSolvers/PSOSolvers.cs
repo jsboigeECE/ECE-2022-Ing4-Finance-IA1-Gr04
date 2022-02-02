7c05b2c6f33cd645d79086428285e14440d6a04f
charles <charles.auvrignon@edu.ece.fr>
2022-02-02 22:16:06 +0100

    Résolution conflit sur branche de Jorge

--- Sudoku.PSOSolvers/PSOSolvers.cs
+++ Sudoku.PSOSolvers/PSOSolvers.cs
@@ -12,25 +12,28 @@ namespace Sudoku.PSOSolvers  //Ceci est un test
     public class PSOSolvers1 : ISolverSudoku //Nous commencerons par coder une classe PSOSolver1 qui fera parti de l'un de nos solvers 
     {
                
-
+        //Taille de la matrice
         public const int taille = 9;
         public const int taille_block = 3;
         static Random rnd = new Random(0);
      
-
+        //Differents type d'organisme
         public enum OrganismType
         {
             Worker,
             Explorer
         }
 
+        //class Organism comportant tous les élèments nécessaires qu'un organisme doit avoir
         public class Organism
         {
+            //Son type
             public OrganismType Type { get; }
-            public int[,] Matrix { get; set; }
+            public int[,] Matrix { get; set; } 
             public int Error { get; set; }
             public int Age { get; set; }
 
+            // constructeur
             public Organism(OrganismType type, int[,] m, int error, int age)
             {
                 Type = type;
@@ -41,18 +44,23 @@ namespace Sudoku.PSOSolvers  //Ceci est un test
         }
 
 
-
+        // Fonction qui resout le sudoku prend en paramètres un Sudoku, le nombre d'organismes et le maximum de paramètres
         private Sudoku SolveInternal(Sudoku sudoku, int numOrganisms, int maxEpochs)
         {
+            //Déterminer le nombre d’objets organisme de travail comme 90 % du nombre total utilisé.
+            //Cette valeur a été déterminée par essais et erreurs
             var numberOfWorkers = (int)(numOrganisms * 0.90);
-            var hive = new Organism[numOrganisms];
+
+
+            var hive = new Organism[numOrganisms]; // creation du tableau ou l'on stock les organismes
 
             var bestError = int.MaxValue;
             Sudoku bestSolution = null;
 
-            for (var i = 0; i < numOrganisms; ++i) //en comm
+            for (var i = 0; i < numOrganisms; ++i) //boucle
             {
-                var organismType = i < numberOfWorkers
+                // Le type depend de la valeur de i 
+                var organismType = i < numberOfWorkers // s'il est inférieur 
                  ? OrganismType.Worker
                   : OrganismType.Explorer;
 
@@ -64,7 +72,7 @@ namespace Sudoku.PSOSolvers  //Ceci est un test
                 if (err >= bestError) continue;
                 bestError = err;
                 bestSolution = Sudoku.New(randomSudoku);
-            } //jusqu'ici
+            } 
 
             var epoch = 0;
             while (epoch < maxEpochs)
@@ -84,6 +92,10 @@ namespace Sudoku.PSOSolvers  //Ceci est un test
                         var neighborError = neighborSudoku.Error;
 
                         var p = rnd.NextDouble();
+
+                        //demande à un objet de l’organisme d’accepter une solution voisine moins bonne que la solution actuelle de l’objet.
+                        //stratégie d’optimisation courants conçue pour aider un algorithme réglés à partir d’une solution non optimal
+
                         if (neighborError < hive[i].Error || p < 0.001)
                         {
                             hive[i].Matrix = PSOSolvers1.DuplicateMatrix(neighbor);
@@ -117,7 +129,10 @@ namespace Sudoku.PSOSolvers  //Ceci est un test
 
 
 
-                // merge best worker with best explorer into worst worker
+                // merge best worker with best explorer into worst worker           
+                //À chaque version de la boucle d’itération, chaque organisme de type Explorateur génère une grille de solution aléatoire.
+                //Une fois que chaque objet organisme a été traité, un nouvel organisme est alors créé
+
                 var bestWorkerIndex = 0;
                 var smallestWorkerError = hive[0].Error;
                 for (var i = 0; i < numberOfWorkers; ++i)
@@ -168,6 +183,10 @@ namespace Sudoku.PSOSolvers  //Ceci est un test
         /// <param name="problem">int[][]</param>
         /// <param name="rand">pass in your random object if you got one</param>
         /// <returns></returns>
+        // merge best worker with best explorer into worst worker
+        //À chaque version de la boucle d’itération, chaque organisme de type Explorateur génère une grille de solution aléatoire.
+        //Une fois que chaque objet organisme a été traité, un nouvel organisme est alors créé
+
         public static int[,] RandomMatrix(Random rnd, int[,] problem)
         {
             var result = DuplicateMatrix(problem);
@@ -218,6 +237,12 @@ namespace Sudoku.PSOSolvers  //Ceci est un test
 
         /// <param name="block">index of a 3x3 block</param>
         /// <returns>returns the (row,column) coordinate of the top-left cell</returns>
+        /// 
+
+        /// Un bloc de méthode accepte un index de ligne r et un index de colonne c et retourne un numéro de bloc (0-8) qui contient la cellule à (r, c).
+        /// Les numéros du bloc sont affectés de gauche à droite, puis de haut en bas. Par exemple, si (r, c) = (3, 8) méthode bloc retourne 5.
+        /// Angle de la méthode accepte un ID de bloc (0-8) et retourne l’index de l’angle supérieur gauche du bloc. Par exemple, si bloc = 8, la méthode renvoie la coin (6, 6).
+
         public static (int row, int column) Corner(int block)
         {
             int r = -1, c = -1;
@@ -265,11 +290,17 @@ namespace Sudoku.PSOSolvers  //Ceci est un test
         }
 
 
+        //En raison de l’invariant grille secondaire 3 x 3, une solution voisine doit se limiter est une permutation d’une grille secondaire.
+        //l’algorithme sélectionne de manière aléatoire un bloc, puis sélectionne les deux cellules dans le bloc
+        //et échange les valeurs dans les deux cellules
+
         public static int[,] NeighborMatrix(Random rnd, int[,] problem, int[,] matrix)
         {
             // pick a random 3x3 block,
             // pick two random cells in block
             // swap values
+
+            /// Creates a copy of the provided matrix
             var result = DuplicateMatrix(matrix);
 
             var block = rnd.Next(0, taille); // [0,8]
@@ -326,14 +357,16 @@ namespace Sudoku.PSOSolvers  //Ceci est un test
 
         /// Generates a matrix (grid, or array of arrays) representing the sudoku puzzle
 
-        /// <param name="rows">number of rows</param>
-        /// <param name="columns">number of columns per row</param>
-        /// <returns>an array of int[][]</returns>
         public static int[,] CreateMatrix(int m, int n)
         {
             var result = new int[m, n];
             return result;
         }
+
+        //accepte deux 9 x 9 matrices de deux objets organisme. La méthode d’analyse par blocs de 0 à 8
+        // Pour chaque bloc, une valeur aléatoire comprise entre 0,0 et 1,0 est générée.
+        //Si la valeur aléatoire est inférieur à 0,50 (c'est-à-dire environ la moitié du temps), puis les valeurs dans les deux blocs sont échangés. 
+
         public static int[,] MergeMatrices(Random rnd, int[,] m1, int[,] m2)
         {
             var result = DuplicateMatrix(m1);
